c813f5879b5075a3e9b0c65996796a95fcaf110b
Andre Philot <andrephilot@gmail.com>
2025-01-15 17:16:21 -0600

    Update LoadingOnSubmission.js
    Adding message to the loading wheel

--- src/LoadingOnSubmission.js
+++ src/LoadingOnSubmission.js
@@ -1,19 +1,22 @@
 function LoadingOnSubmission({ isLoading, buttonText, isFormDirty = true }) {
     return (
         isLoading ? (
-            <div className='loader-container'>
+            <div className="loader-container">
                 <div className="loader"></div>
+                <p className="loading-message">
+                    Hang tight! This might take up to 2 minutes. Think of it as a coffee break, but without the coffee—our free hosting needs its time to shine!
+                </p>
             </div>
         ) : (
-        <button 
-            type="submit" 
-            className="btn btn-primary"
-            disabled={!isFormDirty}
-        >
-            {buttonText}
-        </button>
+            <button 
+                type="submit" 
+                className="btn btn-primary"
+                disabled={!isFormDirty}
+            >
+                {buttonText}
+            </button>
         )
-    )
+    );
 }
 
 export default LoadingOnSubmission;
